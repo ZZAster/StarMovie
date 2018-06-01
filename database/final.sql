e05c52907b40718a74c5c0087ad02ed4783468d3
haibaradu <39399924+haibaradu@users.noreply.github.com>
2018-06-01 18:07:54 +0800

    Update final.sql

--- database/final.sql
+++ database/final.sql
@@ -3,21 +3,20 @@ create database final;
 use final;
 
 
-create TABLE user (userId int unsigned primary key auto_increment,
+create TABLE user (userId int primary key auto_increment,
 				   userName VARCHAR(20) not null,
                    userPassword VARCHAR(45) not null,
                    userEmail VARCHAR(45) not null);
 
-create TABLE movie (movieID int primary key,
+create TABLE movie (movieID int primary key auto_increment,
 					movieTitle VARCHAR(50) not null,
                     movieType VARCHAR(45) not null,
                     movieActor VARCHAR(45) not null,
                     movieDirector VARCHAR(45) not null);
                     
-create TABLE type (typeId int primary key,
+create TABLE type (typeId int primary key auto_increment,
 				   typeName VARCHAR(45) not null);
                    
-
 create TABLE movie_has_type (movie_movieId int not null,
 							 movie_typeId int not null,
                              primary key(movie_movieId, movie_typeId),
@@ -30,24 +29,20 @@ alter TABLE movie add column picPath varchar(200) not null;
 alter TABLE movie add column movieSummary text;
 alter TABLE movie modify movieSummary text not null;
 alter TABLE movie add column movieDate date not null;
-alter TABLE movie add column movieComment text;
 
 
-create TABLE comment (commentId int not null,
-					  comment_userId int not null,
-                      comment_movieId int not null,
-                      primary key(commentId, comment_userId, comment_movieId));
+
+create TABLE rate (rateId int not null auto_increment,
+					  rate_userId int not null,
+                      rate_movieId int not null,
+                      primary key(rateId, rate_userId, rate_movieId)
+                      );
                       
-alter TABLE comment add foreign key(comment_userId) references user(userId);
-drop table comment;
+alter TABLE rate add foreign key(rate_userId) references user(userId);
 
-create TABLE comment (commentId int not null,
-					  comment_userId int unsigned not null,
-                      comment_movieId int not null,
-                      primary key(commentId),
-                      constraint foreign key(comment_userId) references user(userId),
-                      constraint foreign key(comment_movieId) references movie(movieID));
-                      
+alter table rate add foreign key(rate_movieId) references movie(movieID);
+
+
 create TABLE actor (actorId int not null,
 					actorName VARCHAR(45) not null,
 					actorNationality VARCHAR(45) not null,
@@ -71,18 +66,8 @@ create TABLE director_has_movie (director_directorId int not null,
 								 constraint foreign key(director_movieId) references movie(movieId));
                                  
 
-create TABLE list (listId int not null,
-				   list_userId int unsigned not null,
-                   listName VARCHAR(45) not null,
-                   primary key(listId),
-                   constraint foreign key(list_userId) references user(userId));   
-                   
-create TABLE list_has_movie (list_listId int not null,
-							 list_movieId int not null,
-                             primary key(list_listId, list_movieId),
-                             constraint foreign key(list_listId) references list(listId),
-                             constraint foreign key(list_movieId) references movie(movieId));
-
-                      
-
-
+create table user_like_movie(user_userId int not null,
+							user_movieId int not null,
+                            primary key(user_userId, user_movieId),
+                             constraint foreign key(user_userId) references user(userId),
+                             constraint foreign key(user_movieId) references movie(movieId));
